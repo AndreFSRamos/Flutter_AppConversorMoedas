a3eab6d70a9ba4534279fc2d2b48b4c6e65c75ea
André Felipe da Silva Ramos <andre.sis.tec@hotmail.com>
2022-05-22 19:43:09 -0300

    22/05/2022

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -61,97 +61,99 @@ class _HomeState extends State<Home> {
         centerTitle: true,
       ),
       body: FutureBuilder<Map>(
-          //Obetendo as infomarção atravez de uma requisição com a API.
-          future: valueMoeda.getData(),
-          builder: (context, snapshot) {
-            //SWITCH para verificar o resultado da conexão com a API, caso seja
-            //negativo ira informar  a messagem de erro, caso seja positivo ira
-            //exibir a menssagem de carregando dados até a requizição com a API
-            //ser finalizada
+        //Obetendo as infomarção atravez de uma requisição com a API.
+        future: valueMoeda.getData(),
+        builder: (context, snapshot) {
+          //SWITCH para verificar o resultado da conexão com a API, caso seja
+          //negativo ira informar  a messagem de erro, caso seja positivo ira
+          //exibir a menssagem de carregando dados até a requizição com a API
+          //ser finalizada
 
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-              case ConnectionState.waiting:
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return const Center(
+                child: Text(
+                  'Carregando dados...',
+                  style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default:
+              if (snapshot.hasError) {
                 return const Center(
                   child: Text(
-                    'Carregando dados...',
+                    'Erro ao carregar dados...',
                     style: TextStyle(color: Colors.amber, fontSize: 25.0),
                     textAlign: TextAlign.center,
                   ),
                 );
-              default:
-                if (snapshot.hasError) {
-                  return const Center(
-                    child: Text(
-                      'Erro ao carregar dados...',
-                      style: TextStyle(color: Colors.amber, fontSize: 25.0),
-                      textAlign: TextAlign.center,
-                    ),
-                  );
-                } else {
-                  //Selecioando o valor de compra de cada moeda informado no Json recebido do API.
-                  usd = snapshot.data!["results"]["currencies"]["USD"]["buy"];
-                  eur = snapshot.data!["results"]["currencies"]["EUR"]["buy"];
-                  btc = snapshot.data!["results"]["currencies"]["BTC"]["buy"];
+              } else {
+                //Selecioando o valor de compra de cada moeda informado no Json recebido do API.
+                usd = snapshot.data!["results"]["currencies"]["USD"]["buy"];
+                eur = snapshot.data!["results"]["currencies"]["EUR"]["buy"];
+                btc = snapshot.data!["results"]["currencies"]["BTC"]["buy"];
 
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SingleChildScrollView(
-                      child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            const Divider(),
-                            SizedBox(
-                              child: Image.asset('assets/icon/logo.png'),
-                              height: 150,
-                            ),
-                            const Divider(),
-                            const Padding(
-                              padding: EdgeInsets.symmetric(vertical: 10),
-                              child: Text(
-                                'Valores infomados, não devem conter virgulas, letras ou caracteres especiais, apenas ( . ).',
-                                style: TextStyle(
-                                    color: Colors.amber,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                            //Chamando o Widget Text_Convert para cada tipo de moeda e passando seus respectivos parâmetros.
-                            const Divider(),
-                            TextConvet(
-                              nome: 'Real',
-                              simbolo: 'R\$ : ',
-                              acao: _realChanged,
-                              controller: realController,
-                            ),
-                            const Divider(),
-                            TextConvet(
-                              nome: 'Dolar',
-                              simbolo: '\$ : ',
-                              acao: _dolarChanged,
-                              controller: dolarController,
-                            ),
-                            const Divider(),
-                            TextConvet(
-                              nome: 'Euro',
-                              simbolo: '£ : ',
-                              acao: _euroChanged,
-                              controller: euroController,
-                            ),
-                            const Divider(),
-                            TextConvet(
-                              nome: 'BitCoin',
-                              simbolo: 'B : ',
-                              acao: _btcChanged,
-                              controller: btcController,
-                            ),
-                          ]),
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const Divider(),
+                        SizedBox(
+                          child: Image.asset('assets/icon/logo.png'),
+                          height: 150,
+                        ),
+                        const Divider(),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10),
+                          child: Text(
+                            'Valores infomados, não devem conter virgulas, letras ou caracteres especiais, apenas ( . ).',
+                            style: TextStyle(
+                                color: Colors.amber,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        //Chamando o Widget Text_Convert para cada tipo de moeda e passando seus respectivos parâmetros.
+                        const Divider(),
+                        TextConvet(
+                          nome: 'Real',
+                          simbolo: 'R\$ : ',
+                          acao: _realChanged,
+                          controller: realController,
+                        ),
+                        const Divider(),
+                        TextConvet(
+                          nome: 'Dolar',
+                          simbolo: '\$ : ',
+                          acao: _dolarChanged,
+                          controller: dolarController,
+                        ),
+                        const Divider(),
+                        TextConvet(
+                          nome: 'Euro',
+                          simbolo: '£ : ',
+                          acao: _euroChanged,
+                          controller: euroController,
+                        ),
+                        const Divider(),
+                        TextConvet(
+                          nome: 'BitCoin',
+                          simbolo: 'B : ',
+                          acao: _btcChanged,
+                          controller: btcController,
+                        ),
+                      ],
                     ),
-                  );
-                }
-            }
-          }),
+                  ),
+                );
+              }
+          }
+        },
+      ),
     );
   }
 }
